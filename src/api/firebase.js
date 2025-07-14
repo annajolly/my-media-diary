44f2a4b0de4d5f0b84e1dbded30b04f8e6a61e57
Anna Jolly <anna@annajolly.io>
2025-07-13 21:08:30 -0400

    secrets

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -17,11 +17,11 @@ import {
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: 'my-media-diary-auth.firebaseapp.com',
+  projectId: 'my-media-diary-auth',
+  storageBucket: 'my-media-diary-auth.firebasestorage.app',
+  messagingSenderId: '786847608119',
+  appId: '1:786847608119:web:0e8a679e87c3002ebf7eb2',
 };
 
 const app = initializeApp(firebaseConfig);
